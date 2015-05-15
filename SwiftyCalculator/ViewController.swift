e298a9de1a6f0b74c17048e54f58e3452feedd2a
Steve Malsam <stevemalsam@stevemalsam.com>
2015-05-14 19:38:30 -0700

    Initial Commit

---
+++ SwiftyCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyCalculator
+//
+//  Created by Steve Malsam on 5/14/15.
+//  Copyright (c) 2015 Basting Shoebox Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
