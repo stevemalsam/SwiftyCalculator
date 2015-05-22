daadf8e479a6d01136698b3d44c948a3d4432d66
Steve Malsam <stevemalsam@stevemalsam.com>
2015-05-21 19:35:55 -0700

    Added square roots and unary operators.

--- SwiftyCalculator/ViewController.swift
+++ SwiftyCalculator/ViewController.swift
@@ -35,6 +35,7 @@ class ViewController: UIViewController {
         case "÷": performOperation {$1 / $0}
         case "+": performOperation {$0 + $1}
         case "−": performOperation {$1 - $0}
+        case "√": performOperation {sqrt($0)}
         default:break
         }
     }
@@ -42,6 +43,13 @@ class ViewController: UIViewController {
     func performOperation(operation: (Double, Double) -> Double) {
         if operandStack.count >= 2 {
             displayValue = operation(operandStack.removeLast(),operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    private func performOperation(operation: (Double) -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
             enter()
         }
     }
